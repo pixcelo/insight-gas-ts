92ba911db210cdc3f3c20ae77962d972e80dd5c4
tetsu-usptr <bodomu@gmail.com>
2022-04-03 04:05:29 +0900

    privateGetFiatAccounts

--- src/main.js
+++ src/main.js
@@ -14,8 +14,8 @@ const exchangeId = 'liquid',
     });
 
 const SYMBOL = 'BTC/JPY';
-const interval = 3000;
-const LOT = 0.002; // BTC order amount (min 0.0001)
+const interval = 300000; // 5min
+const LOT = 0.005; // BTC order amount (min 0.0001)
 const records = [];
 let orderInfo = null;
 
@@ -36,27 +36,30 @@ let sleep = (ms) => new Promise (resolve => setTimeout (resolve, ms));
             }
             const price = ticker.ask;
             const condition = (records[0] < records[1] && records[1] < records[2]);
-            console.log(`price ${price} records [${records}] condition ${condition}`);
+            console.log(`${getTime(new Date())} price ${price} records [${records}] condition ${condition}`);
 
             // Sell
             if (orderInfo) {
-                const target = orderInfo.price * 1.02;
+                const target = orderInfo.price * 1.01;
 
                 if (price > target) {
                     const order = exchange.createMarketOrder(SYMBOL, 'sell', LOT);
-                    orderInfo = null;
                     writeLog(`sold: profit ${price - orderInfo.price}`);
+                    orderInfo = null;
                     await sleep(interval);
                     continue;
                 }
 
-                console.log(`hold target:${target}`);
+                console.log(` hold target ${roundDecimal(target, 2)}`);
                 await sleep(interval);
                 continue;
             }
 
             // Buy
             if (condition) {
+                if (!await hasEnoughJpy(price * LOT)) {
+                    continue;
+                }
                 const order = exchange.createMarketOrder(SYMBOL, 'buy', LOT);
                 orderInfo = {
                     order: order,
@@ -76,6 +79,22 @@ let sleep = (ms) => new Promise (resolve => setTimeout (resolve, ms));
 
 })();
 
+// 法定通貨残高の取得
+async function hasEnoughJpy(required_balance) {
+    // 個別APIの命名規則 private + {メソッドがgetならGet、postならPost} + {private APIの名前}
+    const data = await exchange.privateGetFiatAccounts();
+    console.log(`jpy ${data[0]['balance']}`);
+    if(data[0]['balance'] >= required_balance) {
+        return true;
+    }
+    console.log('has not enough JPY');
+    return false;
+}
+
+function roundDecimal(value, n) {
+    return Math.round(value * Math.pow(10, n) ) / Math.pow(10, n);
+}
+
 function writeLog(text) {
     const date = new Date().toLocaleString('sv').replace(/\D/g, '');
     text = date + ':' + text + '\n',
@@ -83,4 +102,15 @@ function writeLog(text) {
     fs.appendFileSync('./log/info.log', text, (err) => {
         if (err) throw err;
     });
+}
+
+function getTime(now) {
+    const Year = now.getFullYear();
+    const Month = now.getMonth()+1;
+    const Date = now.getDate();
+    const Hour = now.getHours();
+    const Min = now.getMinutes();
+    const Sec = now.getSeconds();
+
+    return Year + '-' + Month + '-' + Date + '/' + Hour + ':' + Min + ':' + Sec;
 }
